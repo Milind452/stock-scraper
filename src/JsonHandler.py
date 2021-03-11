94031df149ad53a86b43e68d87c093cf88445e48
Milind Mohapatra <mohapatra.milind@gamil.com>
2021-03-12 00:54:25 +0530

    Fix syntax

--- src/JsonHandler.py
+++ src/JsonHandler.py
@@ -14,6 +14,11 @@ def getJsonItem(item, relativePath):
 	jsonObject = getJsonObject(relativePath)
 	return jsonObject[item]
 
+def setJsonObject(jsonObject, relativePath):
+	path = getCurrentWorkingDirectory() + relativePath
+	with open(path, 'w') as outputJson:
+		json.dump(jsonObject, outputJson)
+
 # print(getCurrentWorkingDirectory())
 # print(type(getJsonObject('/../resources/endpoints.json')), type(getJsonItem('home', '/../resources/endpoints.json')))
 # print(getJsonObject('/../resources/endpoints.json'), getJsonItem('home', '/../resources/endpoints.json'))
