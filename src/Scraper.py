29f8432119f2988045750f852682437e5aafb070
Milind Mohapatra <mohapatra.milind@gamil.com>
2021-03-12 19:44:57 +0530

    Create basic framework structure

---
+++ src/Scraper.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+class scraper:
+	def __init__(self, url):
+		pass
+
+	def getHtml(self):
+		pass
+
+	def getSoup(self):
+		pass
+
+	def getTitle(self):
+		pass
+
+	def getHead(self):
+		pass
+
+	def getBody(self):
+		pass
