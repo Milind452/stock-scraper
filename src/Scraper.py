d2924f5294fc60a15bb954bb9bc465e73696c234
Milind Mohapatra <mohapatra.milind@gamil.com>
2021-03-12 19:58:59 +0530

    Add test function

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-class scraper:
+class Scraper:
 	def __init__(self, url):
 		self.url = url
 
@@ -12,10 +12,19 @@ class scraper:
 		return BeautifulSoup(self.getHtml().content, 'html.parser')
 
 	def getTitle(self):
-		return self.getSoup.title.text
+		return self.getSoup().title.text
 
 	def getHead(self):
-		return self.getSoup.head.text
+		return self.getSoup().head.text
 
 	def getBody(self):
-		return self.getSoup.body.text
+		return self.getSoup().body.text
+
+if __name__ == '__main__':
+	scraper = Scraper("https://www.moneycontrol.com/stocksmarketsindia/")
+	print(scraper.getTitle())
+	print('**************************')
+	print(scraper.getHead())
+	print('**************************')
+	print(scraper.getBody())
+
