993d4d8398038920f08dbaecf5438727c942874e
Milind Mohapatra <mohapatra.milind@gamil.com>
2021-03-12 21:45:36 +0530

    Implement Scrapper framework

--- src/dummy.py
+++ src/dummy.py
@@ -1,40 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-
+import Scraper
 import JsonHandler
 
-home = JsonHandler.getJsonItem('home', '/../resources/endpoints.json')
-page = requests.get(home)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-pageTitle = soup.title.text
-pageHead = soup.head
-pageBody = soup.body.text
-
-# print(pageTitle)
-# print("********************************")
-# print(pageHead)
-# print("********************************")
-# print(pageBody)
-
-table = soup.select('table')[1].text
-
-table = list(table.split('\n'))
-temp = list()
-tmp = list()
-counter = 1
-for element in table:
-    if element == '':
-        continue
-    tmp.append(element)
-    if counter % 4 == 0:
-        temp.append(tmp)
-        tmp = list()
-    counter += 1
-
-indices = dict()
-for x in range(1, len(temp)):
-    indices[temp[x].pop(0)] = temp[x]
+url = JsonHandler.getJsonItem('home', '/../resources/endpoints.json')
+scraper = Scraper.Scraper(url)
+indices = scraper.getTableByIndex(1)
 
 print(indices)
 JsonHandler.setJsonObject(indices, "/../target/indices.json")
